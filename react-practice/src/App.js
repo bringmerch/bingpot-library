caf18af774f3511b4f51e6460f446d8d4569e23d
bringmerch <lgop8327@gmail.com>
2026-07-13 18:01:11 +0900

    배열

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,51 +1,89 @@
 import './App.css';
+import {useState} from 'react'; // useState는 react에서 기본적으로 제공하는 Hook이다.
 
-// 사용자 정의 태그 만드는 법 : 함수 만든다.
-function Header(props) { // props = {title: "REACT!!"}
-  console.log(props);
-  return ( // 괄호로 감싸서 return 해야된다.
-    <header>
-          <h1><a href="/">{props.title}</a></h1>
-    </header>
-)
-}
 
-function Nav() {
-  return (
-      <nav>
-        <ol>
-          <li>
-            <a href="/read/1">html</a>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-          <li>
-            <a href="/read/3">js</a>
-          </li>
-        </ol>
-       </nav>
-  )
-}
+
 function Article(props) {
   return (
       <article>
       <h2>{props.title}</h2>
-        {props.content}
+        {props.body}
       </article>
   )
 }
 
+
+// 사용자 정의 태그 만드는 법 : 함수 만든다.
+function Header(props) { // props = {title: "REACT!!"}
+    console.log(props);
+    return ( // 태그리턴 : 괄호로 감싸서 return 해야된다.
+        <header>
+            <h1><a href="/" onClick={(event) =>  {
+                event.preventDefault();
+                props.onChangeMode(); // onChangeMode로 넘긴 함수 실행
+            }}>{props.title}</a></h1> {/* 기본 html이 아니다. onClick <- 주목 */}
+
+        </header>
+    )
+}
+
+function Nav(props) {
+    const lis = [
+    ]
+
+    for (let i=0; i<props.topics.length; i++) {
+        let t= props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a href={'/read/' + t.id}
+                   id={t.id}
+                   onClick={event => {
+                    event.preventDefault();
+                    props.onChangeMode(event.target.id); // target : 이벤트를 유발시킨 태그 <a>
+                   }}>
+                    {t.title}
+                </a>
+            </li>
+        );
+    }
+
+    return (
+        <nav>
+            <ol>
+                {lis}
+            </ol>
+        </nav>
+    )
+}
+
 function App() {
-  return (
-    <div>
-      <Header title="REACT!!"></Header>
-      <Header title="hello!"></Header>
-      <Header title="me likey!!!!!"></Header>
-      <Nav></Nav>
-      <Article title="article's title." content={"article's content."}></Article>
-    </div>
-  );
+    const mode = 'WELCOME';
+    // ㄴ 얘가 바뀔 때 App()이 한번더실행되면서 태그가 바뀌는 걸 하고 싶다 -> useState 사용
+    const topics = [
+          {id: 1, title: 'html', body:'html is...'},
+          {id: 2, title: 'css', body:'css is...'},
+          {id: 3, title: 'js', body:'js is...'},
+      ];
+
+    let content = null;
+
+    if (mode === "WELCOME") {
+        content = <Article title="Welcome" body="Hello, WEB"></Article>;
+    } else if (mode === "READ") {
+        content = <Article title="Read" body="Hello, Reader"></Article>;
+    }
+
+    return (
+      <div>
+        <Header title="REACT!!" onChangeMode={()=> {
+            // mode = 'WELCOME'; -> 일케하면 안되는이유 : App 함수는 변하지 않기 때문에 Return 값에는 변화가 없다.
+        }}></Header>
+        <Nav topics={topics} onChangeMode={() => {
+            mode='READ';
+        }}></Nav>  {/* 표현식: 중괄호로 표현 */}
+          {content}
+      </div>
+    );
 }
 
 export default App;
